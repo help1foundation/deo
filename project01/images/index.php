9ee08d931868848eb370939f31796a5d8eee5c67
amjad <help1foundation@gmail.com>
2013-12-27 13:25:10 +0100

    amjad01

---
+++ project01/images/index.php
@@ -0,0 +1,265 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Untitled Page</title>
+<meta name="generator" content="WYSIWYG Web Builder 9 Trial Version - http://www.wysiwygwebbuilder.com">
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+<style type="text/css">
+div#container
+{
+   width: 320px;
+   position: relative;
+   margin-top: 0px;
+   margin-left: auto;
+   margin-right: auto;
+   text-align: left;
+}
+body
+{
+   background-color: #FFFFFF;
+   color: #000000;
+   font-family: Arial;
+   font-size: 13px;
+   margin: 0;
+   text-align: center;
+}
+</style>
+<style type="text/css">
+a
+{
+   color: #0000FF;
+   text-decoration: underline;
+}
+a:visited
+{
+   color: #800080;
+}
+a:active
+{
+   color: #FF0000;
+}
+a:hover
+{
+   color: #0000FF;
+   text-decoration: underline;
+}
+</style>
+<style type="text/css">
+#wb_Text1 
+{
+   background-color: transparent;
+   border: 0px #000000 solid;
+   padding: 0;
+   text-align: center;
+}
+#wb_Text1 div
+{
+   text-align: center;
+}
+#Image1
+{
+   border: 0px #000000 solid;
+}
+#wb_Text2 
+{
+   background-color: transparent;
+   border: 0px #000000 solid;
+   padding: 0;
+   text-align: left;
+}
+#wb_Text2 div
+{
+   text-align: left;
+}
+#wb_Text3 
+{
+   background-color: transparent;
+   border: 0px #000000 solid;
+   padding: 0;
+   text-align: center;
+}
+#wb_Text3 div
+{
+   text-align: center;
+}
+#wb_CssMenu1
+{
+   border: 0px #0000FF solid;
+   background-color: transparent;
+}
+#wb_CssMenu1 ul
+{
+   list-style-type: none;
+   margin: 0;
+   padding: 0;
+   position: relative;
+   display: inline-block;
+}
+#wb_CssMenu1 li
+{
+   float: left;
+   margin: 0;
+   padding: 0px 4px 0px 0px;
+   width: 280px;
+}
+#wb_CssMenu1 a
+{
+   display: block;
+   float: left;
+   color: #FFFFFF;
+   border: 1px #6A5ACD solid;
+   background-color: #0000FF;
+   background-image: none;
+   font-family: "Courier New";
+   font-size: 21px;
+   font-weight: bold;
+   font-style: normal;
+   text-decoration: none;
+   width: 268px;
+   height: 26px;
+   padding: 0px 5px 0px 5px;
+   vertical-align: middle;
+   line-height: 26px;
+   text-align: center;
+}
+#wb_CssMenu1 li:hover a, #wb_CssMenu1 a:hover
+{
+   color: #FFFFFF;
+   background-color: #191970;
+   background-image: none;
+   border: 1px #483D8B solid;
+}
+#wb_CssMenu1 li.firstmain
+{
+   padding-left: 0px;
+}
+#wb_CssMenu1 li.lastmain
+{
+   padding-right: 0px;
+}
+#wb_CssMenu1 li:hover, #wb_CssMenu1 li a:hover
+{
+   position: relative;
+}
+#wb_CssMenu1 a.withsubmenu
+{
+   padding: 0 5px 0 5px;
+   width: 268px;
+   background-image: none;
+}
+#wb_CssMenu1 li:hover a.withsubmenu, #wb_CssMenu1 a.withsubmenu:hover
+{
+   background-image: none;
+}
+#wb_CssMenu1 ul ul
+{
+   position: absolute;
+   left: -9999px;
+   top: -9999px;
+   width: 280px;
+   height: auto;
+   border: none;
+   background-color: transparent;
+}
+#wb_CssMenu1 ul :hover ul
+{
+   left: 0px;
+   top: 28px;
+   padding-top: 0px;
+}
+#wb_CssMenu1 .firstmain:hover ul
+{
+   left: 0px;
+}
+#wb_CssMenu1 li li
+{
+   width: 280px;
+   padding: 0px 0px 0px 0px;
+   border: 0px #1E90FF solid;
+   border-width: 0 0px;
+}
+#wb_CssMenu1 li li.firstitem
+{
+   border-top: 0px #1E90FF solid;
+}
+#wb_CssMenu1 li li.lastitem
+{
+   border-bottom: 0px #1E90FF solid;
+}
+#wb_CssMenu1 ul ul a, #wb_CssMenu1 ul :hover ul a
+{
+   float: none;
+   margin: 0;
+   width: 266px;
+   height: auto;
+   white-space: normal;
+   padding: 4px 6px 3px 6px;
+   background-color: #4169E1;
+   background-image: none;
+   border: 1px #000000 solid;
+   color: #FFFFFF;
+   font-family: "Courier New";
+   font-size: 21px;
+   font-weight: bold;
+   font-style: normal;
+   line-height: 21px;
+   text-align: center;
+   text-decoration: none;
+}
+#wb_CssMenu1 ul :hover ul .firstitem a
+{
+   margin-top: 0px;
+}
+#wb_CssMenu1 ul ul :hover a, #wb_CssMenu1 ul ul a:hover, #wb_CssMenu1 ul ul :hover ul :hover a, #wb_CssMenu1 ul ul :hover ul a:hover
+{
+   background-color: #D8BFD8;
+   background-image: none;
+   border: 1px #696969 solid;
+   color: #FFFFFF;
+}
+#wb_CssMenu1 br
+{
+   clear: both;
+   font-size: 1px;
+   height: 0;
+   line-height: 0px;
+}
+</style>
+</head>
+<body>
+<div id="container">
+<div id="wb_Shape2" style="position:absolute;left:20px;top:669px;width:272px;height:54px;z-index:0;">
+<img src="images/img0002.png" id="Shape2" alt="" style="border-width:0;width:272px;height:54px;"></div>
+<a href="http://www.wysiwygwebbuilder.com" target="_blank"><img src="images/builtwithwwb9.png" alt="WYSIWYG Web Builder" style="position:absolute;left:453px;top:735px;width:88px;height:31px;border-width:0;z-index:250"></a>
+<div id="wb_Text1" style="position:absolute;left:30px;top:22px;width:250px;height:24px;text-align:center;z-index:2;">
+<span style="color:#0000CD;font-family:Arial;font-size:20px;"><strong>MOBILE ZONE</strong></span></div>
+<div id="wb_Image1" style="position:absolute;left:20px;top:184px;width:280px;height:202px;z-index:3;">
+<img src="images/DSC00027.JPG" id="Image1" alt="" style="width:280px;height:202px;"></div>
+<div id="wb_Text2" style="position:absolute;left:36px;top:400px;width:264px;height:128px;z-index:4;text-align:left;">
+<span style="color:#000000;font-family:Arial;font-size:13px;">Name:&nbsp;&nbsp; AMJAD ALI<br>NGO:&nbsp;&nbsp;&nbsp; DAIRY AND RURAL FOUNDATION<br>&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; (USAID PROJECT)<br><br>Admin:&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; Atif Awan<br>Account:&nbsp;&nbsp;&nbsp; Raees Khan<br>Store kep:&nbsp; Rafaqat Khan<br>DEO:&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; Amjad Ali </span></div>
+<div id="wb_Shape1" style="position:absolute;left:59px;top:572px;width:188px;height:32px;z-index:5;">
+<img src="images/img0001.png" id="Shape1" alt="" style="border-width:0;width:188px;height:32px;"></div>
+<div id="wb_Text3" style="position:absolute;left:26px;top:673px;width:250px;height:38px;text-align:center;z-index:6;">
+<span style="color:#FFFFFF;font-family:Arial;font-size:16px;"><strong>Copyright @2013<br>all right reserved</strong></span></div>
+<div id="wb_CssMenu1" style="position:absolute;left:20px;top:78px;width:284px;height:91px;text-align:center;z-index:7;">
+<ul>
+<li class="firstmain"><a class="withsubmenu" href="#" target="_self">ITEMS</a>
+
+<ul>
+<li class="firstitem"><a href="#" target="_self">HOME</a>
+</li>
+<li><a href="#" target="_self">PICTURE</a>
+</li>
+<li><a href="#" target="_self">ITEM&nbsp;PRICE</a>
+</li>
+<li class="lastitem"><a href="#" target="_self">CONTACT</a>
+</li>
+</ul>
+</li>
+</ul>
+<br>
+</div>
+</div>
+</body>
+</html>
